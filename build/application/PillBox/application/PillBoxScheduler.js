7ed116b0f30421e1fcec785553f91a187b333e03
Damian Alanis Ramirez <damian.alanis98@gmail.com>
2021-06-15 20:08:27 -0500

    Scheduler

---
+++ build/application/PillBox/application/PillBoxScheduler.js
@@ -0,0 +1,24 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const moment_1 = __importDefault(require("moment"));
+const node_cron_1 = require("node-cron");
+class PillBoxScheduler {
+    constructor() {
+        this.run = () => {
+            node_cron_1.schedule('* * * * * ', () => {
+                const dateToCompare = '2021-06-15T01:10:00';
+                if (this.dateIsInThePast(dateToCompare))
+                    console.log('Date has passed');
+                else
+                    console.log('Date has nos passed');
+            });
+        };
+        this.dateIsInThePast = (dateISOString) => {
+            return moment_1.default(dateISOString).isAfter(new Date().toISOString());
+        };
+    }
+}
+exports.default = PillBoxScheduler;
---
+++ src/application/PillBox/application/PillBoxScheduler.ts
@@ -0,0 +1,19 @@
+import moment from 'moment';
+import { schedule } from 'node-cron';
+
+
+export default class PillBoxScheduler {
+
+    public run = () => {
+        schedule('* * * * * ', () => {
+            const dateToCompare = '2021-06-15T01:10:00';
+            if(this.dateIsInThePast(dateToCompare))
+                console.log('Date has passed')
+            else console.log('Date has nos passed')
+        })
+    }
+
+    private dateIsInThePast = (dateISOString: string) => {
+        return moment(dateISOString).isAfter(new Date().toISOString());
+    }
+}
